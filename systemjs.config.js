e6e529bc8c2c5406b07e77aaf0adc77e7b1d14c2
NamitaMalik <namita1990@gmail.com>
2016-10-24 13:49:08 +0530

    Updating to angular 2.1

--- systemjs.config.js
+++ systemjs.config.js
@@ -22,8 +22,8 @@
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             // other libraries
-            'rxjs': 'npm:rxjs',
-            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api'
+            'rxjs':                      'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -33,10 +33,6 @@
             },
             rxjs: {
                 defaultExtension: 'js'
-            },
-            '@angular/material': {
-                format: 'cjs',
-                main: 'material.umd.js'
             },
             'angular-in-memory-web-api': {
                 main: './index.js',
